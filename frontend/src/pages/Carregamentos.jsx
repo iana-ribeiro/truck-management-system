1dff35f0b7409a7f74ec7b3a4dbf792c064a8630
Iana Ribeiro <ianaribeiro.tech@gmail.com>
2026-07-21 23:05:00 -0300

    20260721

--- frontend/src/pages/Carregamentos.jsx
+++ frontend/src/pages/Carregamentos.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'; //"React, vou usar memória e vou executar um código quando a página abrir."
+import { useEffect, useState } from 'react';
 import BarraPesquisa from '../components/BarraPesquisa/BarraPesquisa';
 import Header from '../components/Header/Header';
 import Layout from '../components/Layout/Layout';
@@ -8,14 +8,18 @@ import { buscarCarregamentos } from '../services/carregamentos'; // Traz a funç
 
 // useEffect = "Execute este código quando alguma coisa acontecer." Executa quando o React decide que é a hora (por exemplo, ao abrir a página).
 
+// useState = "React, vou usar memória."
+
 function Carregamentos() {
-  const [carregamentos, setCarregamentos] = useState([]);
+  const [carregamentos, setCarregamentos] = useState([]); // carregamentos = É uma variável, guarda os dados da tabela. setCarregamentos = É a função que altera essa variável. Analogia com o controle remoto.
 
   useEffect(() => {
+    // Execute o código depois que a página foi renderizada. "Buscar os carregamentos na API."
     async function carregarDados() {
-      const dados = await buscarCarregamentos();
+      // Essa função organiza o processo. "Vou buscar os dados (1) e depois colocá-los no estado (2)."
+      const dados = await buscarCarregamentos(); // (1) "Conversar com a API."
 
-      setCarregamentos(dados);
+      setCarregamentos(dados); //(2)
 
       console.log(dados);
     }
@@ -24,6 +28,7 @@ function Carregamentos() {
   }, []);
 
   return (
+    // Devolve a interface da página.
     <Layout>
       <Header />
 
